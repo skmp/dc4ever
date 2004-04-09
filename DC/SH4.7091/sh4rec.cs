072ec15f514fb817d082591c219a62b714af24a1
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2004-04-09 09:03:49 +0200

    Modified sh4rec.cs (dc4ever-2003)

---
+++ DC/SH4.7091/sh4rec.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace DC4Ever.DC.SH4._7091
+{
+	/// <summary>
+	/// sh4 Recompiler.
+	/// </summary>
+	public class sh4rec
+	{
+		static uint recpc;
+		//find opcode block
+		static void procPC()
+		{//get opcodes while not branch /jump or max block len
+			
+		}
+		static void recompilePC()
+		{//recompile a opcode
+
+		}
+
+	}
+}
