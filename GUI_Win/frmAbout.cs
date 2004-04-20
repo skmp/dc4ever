033f32539354891bff3208b0a8a8efda24df244c
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2004-04-20 10:40:42 +0200

    Modified frmAbout.cs (DC4Ever_mncsh)

---
+++ GUI_Win/frmAbout.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace DC4Ever
+{
+	/// <summary>
+	/// Summary description for frmAbout.
+	/// </summary>
+	public class frmabout : System.Windows.Forms.Form
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public frmabout()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+
+			//
+			// 
+			//
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// frmabout
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(292, 266);
+			this.Name = "frmabout";
+			this.Text = "frmAbout";
+			this.Load += new System.EventHandler(this.frmabout_Load);
+
+		}
+		#endregion
+
+		private void frmabout_Load(object sender, System.EventArgs e)
+		{
+		
+		}
+
+	}
+}
