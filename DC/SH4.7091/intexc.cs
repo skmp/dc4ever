9b2ce185861af9e93877369558938f2c94ce6503
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2004-11-03 13:01:38 +0100

    Modified intexc.cs (DC4Ever_mncsh)

--- DC/SH4.7091/intexc.cs
+++ DC/SH4.7091/intexc.cs
@@ -1,3 +1,13 @@
+//This is a part of the DC4Ever emulator
+//You are not allowed to release modified(or unmodified) versions
+//without asking me (drk||Raziel).
+//For Suggestions ect please e-mail at : stef_mp@yahoo.gr
+//Note : This is just to prove that Fast emulation is possible with 
+//	C# /.net ...And yes , this code could be writen at VB.net and 
+//	run more or less at the same speed on dynarec mode
+//	This code requires C#.net 2.0 (Get the C# epxress 2005 Beta from microsoft)
+//
+
 using System;
 
 namespace DC4Ever
@@ -5,7 +15,7 @@ namespace DC4Ever
 	/// <summary>
 	/// Interups ,Exeptions ,Resets Handling
 	/// </summary>
-    public partial class emu
+    public static partial class emu
     {
 
 	}
