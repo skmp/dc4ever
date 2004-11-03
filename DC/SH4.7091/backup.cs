7a6683f402e65cdb3f882e7bb234ddb515cfccc0
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2004-11-03 12:01:38 +0100

    Modified backup.cs (dc4ever-2003)

--- DC/SH4.7091/backup.cs
+++ DC/SH4.7091/backup.cs
@@ -1,3 +1,13 @@
+//This is a part of the DC4Ever emulator
+//You are not allowed to release modified(or unmodified) versions
+//without asking me (drk||Raziel).
+//For Suggestions ect please e-mail at : stef_mp@yahoo.gr
+//Note : This is just to prove that Fast emulation is possible with 
+//	C# /.net ...And yes , this code could be writen at VB.net and 
+//	run more or less at the same speed on dynarec mode
+//	This code requires C#.net 2.0 (Get the C# epxress 2005 Beta from microsoft)
+//
+
 #region oldcode
 #if ba
 		public static void runcpu()
