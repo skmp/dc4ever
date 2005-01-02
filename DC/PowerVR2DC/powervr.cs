f40d3bc8124b4e3db700fc5b32eb64a59421d91b
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2005-01-02 23:33:10 +0100

    Modified powervr.cs (DC4Ever_mncsh)

--- DC/PowerVR2DC/powervr.cs
+++ DC/PowerVR2DC/powervr.cs
@@ -1,3 +1,14 @@
+
+//This is a part of the DC4Ever emulator
+//You are not allowed to release modified(or unmodified) versions
+//without asking me (drk||Raziel).
+//For Suggestions ect please e-mail at : stef_mp@yahoo.gr
+//Note : This is just to prove that Fast emulation is possible with 
+//	C# /.net ...And yes , this code could be writen at VB.net and 
+//	run more or less at the same speed on dynarec mode
+//	This code requires C#.net 2.0 (Get the C# epxress 2005 Beta from microsoft)
+//
+
 using System;
 using System.Drawing;
 using System.Runtime.InteropServices;
@@ -6,7 +17,7 @@ namespace DC4Ever
 	/// <summary>
 	/// Summary description for 
 	/// </summary>
-    public partial class emu
+    public static partial class emu
     {
 		#region WinApi
 		public struct BITMAPINFOHEADER
@@ -63,11 +74,11 @@ namespace DC4Ever
 			}
 			else 
 			{
-				//dc.dcon.WriteLine("Address read out of Vram on write (pc="+pc+")");
+				dc.dcon.WriteLine("Address read out of Vram on write (pc="+pc+")");
 				return;
 			}
 			#endregion
-		switch (len)
+		    switch (len)
 			{
 				case 0x1://1 byte write
 					vram[adr]=(byte)data;
@@ -123,6 +134,7 @@ namespace DC4Ever
 		
 		public static unsafe  void present()// draw the framebuffer(640*480*16 bit)
 		{
+            #if nrt
                 System.IntPtr hdc = dx.bb.GetDc();
                 fixed (byte* i = &vram[0])
                 {
@@ -130,12 +142,9 @@ namespace DC4Ever
                         StretchDIBits(hdc, 0, 0, 640, 480, 0, 0, 640, 480, i, bi, 0, 13369376);
                 }
                 dx.bb.ReleaseDc(hdc);
-                try
-                {
-                    dx.fb.Draw(new Rectangle(dc.frmMain.PointToScreen(new Point(dc.frmMain.ClientRectangle.X + 8, dc.frmMain.ClientRectangle.Y + 8))
-                        , new Size(dc.frmMain.screen.Width, dc.frmMain.screen.Height)), dx.bb, Microsoft.DirectX.DirectDraw.DrawFlags.DoNotWait | Microsoft.DirectX.DirectDraw.DrawFlags.Async);
-                }
-                catch { }
+                dx.fb.Draw(new Rectangle(dc.frmMain.PointToScreen(new Point(dc.frmMain.ClientRectangle.X + 8, dc.frmMain.ClientRectangle.Y + 8))
+                    , new Size(dc.frmMain.screen.Width, dc.frmMain.screen.Height)), dx.bb, Microsoft.DirectX.DirectDraw.DrawFlags.DoNotWait | Microsoft.DirectX.DirectDraw.DrawFlags.Async);
+#endif
             fps+=1;
 		}
 	}
