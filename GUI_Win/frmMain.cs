3e18dadabd4d1f83d5c702cf9aa9ce9a79a3dd18
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2005-06-10 01:44:00 +0200

    Modified frmMain.cs (DC4Ever_meh)

--- GUI_Win/frmMain.cs
+++ GUI_Win/frmMain.cs
@@ -21,9 +21,6 @@ namespace DC4Ever
 		public System.Windows.Forms.PictureBox screen;
 		private System.Windows.Forms.Label label1;
         private System.Windows.Forms.MenuItem menuItem3;
-        private TrackBar trackBar1;
-        private Label label2;
-        private Label label3;
         private System.ComponentModel.IContainer components;
 
 		public frmmain()
@@ -63,123 +60,159 @@ namespace DC4Ever
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.components = new System.ComponentModel.Container();
-            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
-            this.menuItem1 = new System.Windows.Forms.MenuItem();
-            this.menuItem2 = new System.Windows.Forms.MenuItem();
-            this.menuItem3 = new System.Windows.Forms.MenuItem();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.screen = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.screen)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            this.SuspendLayout();
-// 
-// mainMenu1
-// 
-            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
-                this.menuItem1
-            });
-            this.mainMenu1.Name = "mainMenu1";
-// 
-// menuItem1
-// 
-            this.menuItem1.Index = 0;
-            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+			this.components = new System.ComponentModel.Container();
+			this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+			this.menuItem1 = new System.Windows.Forms.MenuItem();
+			this.menuItem2 = new System.Windows.Forms.MenuItem();
+			this.menuItem3 = new System.Windows.Forms.MenuItem();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.screen = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.screen)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// mainMenu1
+			// 
+			this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1});
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = 0;
+			this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
             this.menuItem2,
             this.menuItem3});
-            this.menuItem1.Name = "menuItem1";
-            this.menuItem1.Text = "Main";
-// 
-// menuItem2
-// 
-            this.menuItem2.Index = 0;
-            this.menuItem2.Name = "menuItem2";
-            this.menuItem2.Text = "Run";
-            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
-// 
-// menuItem3
-// 
-            this.menuItem3.Index = 1;
-            this.menuItem3.Name = "menuItem3";
-            this.menuItem3.Text = "Run Cached";
-            this.menuItem3.Visible = false;
-            this.menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
-// 
-// timer1
-// 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-// 
-// screen
-// 
-            this.screen.Location = new System.Drawing.Point(8, 1);
-            this.screen.Name = "screen";
-            this.screen.Size = new System.Drawing.Size(640, 480);
-            this.screen.TabIndex = 0;
-            this.screen.TabStop = false;
-// 
-// label1
-// 
-            this.label1.Location = new System.Drawing.Point(8, 488);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(640, 24);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-// 
-// trackBar1
-// 
-            this.trackBar1.Location = new System.Drawing.Point(8, 519);
-            this.trackBar1.Maximum = 382;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(636, 45);
-            this.trackBar1.TabIndex = 2;
-            this.trackBar1.Value = 35;
-            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
-// 
-// label2
-// 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(535, 507);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 14);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Unrolling depth :";
-// 
-// label3
-// 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(622, 507);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(17, 14);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "35";
-// 
-// frmmain
-// 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(656, 564);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.screen);
-            this.Menu = this.mainMenu1;
-            this.Name = "frmmain";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Dc4Ever v 1.0 - beta3 - Managed Recompiler - Static Condtional/Uncoditional brach" +
-                " inlining";
-            ((System.ComponentModel.ISupportInitialize)(this.screen)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.menuItem1.Text = "Main";
+			// 
+			// menuItem2
+			// 
+			this.menuItem2.Index = 0;
+			this.menuItem2.Text = "Run";
+			this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+			// 
+			// menuItem3
+			// 
+			this.menuItem3.Index = 1;
+			this.menuItem3.Text = "Run Cached";
+			this.menuItem3.Visible = false;
+			this.menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// screen
+			// 
+			this.screen.Location = new System.Drawing.Point(8, 1);
+			this.screen.Name = "screen";
+			this.screen.Size = new System.Drawing.Size(640, 480);
+			this.screen.TabIndex = 0;
+			this.screen.TabStop = false;
+			this.screen.Click += new System.EventHandler(this.screen_Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(8, 488);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(640, 24);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "label1";
+			// 
+			// frmmain
+			// 
+			this.ClientSize = new System.Drawing.Size(656, 508);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.screen);
+			this.Menu = this.mainMenu1;
+			this.Name = "frmmain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Dc4Ever v 1.0 - beta3 - Managed Recompiler - Static Condtional/Uncoditional brach" +
+				" inlining";
+			this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmmain_KeyUp);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmmain_KeyDown);
+			((System.ComponentModel.ISupportInitialize)(this.screen)).EndInit();
+			this.ResumeLayout(false);
 
         }
+
+
+
 		#endregion
+
+		public static ushort kcode = 0xFFFF;
+		void frmmain_KeyUp(object sender, KeyEventArgs e)
+		{
+			switch (e.KeyCode)
+			{
+				case Keys.Z:
+					kcode |= emu.key_CONT_A;
+					break;
+				case Keys.X:
+					kcode |= emu.key_CONT_B;
+					break;
+				case Keys.C:
+					kcode |= emu.key_CONT_C;
+					break;
+				case Keys.V:
+					kcode |= emu.key_CONT_D;
+					break;
+
+				case Keys.ShiftKey:
+					kcode |= emu.key_CONT_START;
+					break;
+
+				case Keys.Up:
+					kcode |= emu.key_CONT_DPAD_UP;
+					break;
+				case Keys.Down:
+					kcode |= emu.key_CONT_DPAD_DOWN;
+					break;
+				case Keys.Left:
+					kcode |= emu.key_CONT_DPAD_LEFT;
+					break;
+				case Keys.Right:
+					kcode |= emu.key_CONT_DPAD_RIGHT;
+					break;
+			}
+		}
+
+		void frmmain_KeyDown(object sender, KeyEventArgs e)
+		{
+			switch (e.KeyCode)
+			{
+				case Keys.Z:
+					kcode &= 0xFFFF - emu.key_CONT_A;
+					break;
+				case Keys.X:
+					kcode &= 0xFFFF - emu.key_CONT_B;
+					break;
+				case Keys.C:
+					kcode &= 0xFFFF - emu.key_CONT_C;
+					break;
+				case Keys.V:
+					kcode &= 0xFFFF - emu.key_CONT_D;
+					break;
+
+				case Keys.ShiftKey:
+					kcode &= 0xFFFF - emu.key_CONT_START;
+					break;
+
+				case Keys.Up:
+					kcode &= 0xFFFF - emu.key_CONT_DPAD_UP;
+					break;
+				case Keys.Down:
+					kcode &= 0xFFFF - emu.key_CONT_DPAD_DOWN;
+					break;
+				case Keys.Left:
+					kcode &= 0xFFFF - emu.key_CONT_DPAD_LEFT;
+					break;
+				case Keys.Right:
+					kcode &= 0xFFFF - emu.key_CONT_DPAD_RIGHT;
+					break;
+			}
+		}
 
 		private void menuItem1_Click(object sender, System.EventArgs e)
 		{
@@ -201,8 +234,9 @@ namespace DC4Ever
 
         private void timer1_Tick(object sender, System.EventArgs e)
         {
-            this.label1.Text = "Running at " + System.Convert.ToString(((double)emu.opcount / 1024 / 1024) / ((double)(System.DateTime.Now.Ticks - told) / 10000000)) + " MHz , " + emu.fps + " fps(not real, just screen refresh) ;vram:" + (emu.mw>>20).ToString()+ " mb";// + ((float)emu.mw / 1024 / 1204).ToString() + " megabyte vram writes per sec " + emu.ch.ToString() + ",cache hits " + emu.cm.ToString() + ",cache misses " + ((emu.ch + 1) / (emu.cm + 1)).ToString() + ":1 cache hit ratio " + ;
-            emu.opcount = 0;
+            this.label1.Text = "Running at " + System.Convert.ToString(((double)emu.clcount / 1024 / 1024) / ((double)(System.DateTime.Now.Ticks - told) / 10000000)) + " MHz , " + emu.fps + " fps(not real, just screen refresh) ;vram:" + (emu.mw).ToString() + " b;pc=" + Convert.ToString(emu.pc, 16); ;// + ((float)emu.mw / 1024 / 1204).ToString() + " megabyte vram writes per sec " + emu.ch.ToString() + ",cache hits " + emu.cm.ToString() + ",cache misses " + ((emu.ch + 1) / (emu.cm + 1)).ToString() + ":1 cache hit ratio " + ;
+            //dc.dcon.WriteLine(emu.pc);
+            emu.clcount = 0;
             emu.fps = 0;
             //emu.mw = 0;
             told = System.DateTime.Now.Ticks;
@@ -218,18 +252,11 @@ namespace DC4Ever
 			//runcpuDyna();
 		}
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-#if !interpreter
-            emu.br_8_b_level_max = (uint)trackBar1.Value;
-            emu.br_8_f_level_max = (uint)trackBar1.Value;
-            label3.Text = trackBar1.Value.ToString();
-            for (int i = 0; i < emu.RecMB; i++) 
-            {
-                emu.DynaCache[i].BaseAddress = 0;
-            }
-#endif
-        }
+		private void screen_Click(object sender, EventArgs e)
+		{
+
+		}
+
 
 
     }
