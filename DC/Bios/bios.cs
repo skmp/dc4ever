f9fe2591bddbf02c9d14374793d852aa0f8372ea
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2005-06-14 15:35:57 +0200

    Modified bios.cs (DC4Ever_meh)

--- DC/Bios/bios.cs
+++ DC/Bios/bios.cs
@@ -15,17 +15,26 @@ namespace DC4Ever
 	/// Emulates the 
 	/// HLE Syscall emulation
 	/// </summary>
-	#if nrt 
     public unsafe static partial  class emu
- #else  
-    public unsafe partial  class emu 
- #endif
 	{
+		const uint dc_bios_hle_vec_1 = 0x100;
+		const uint dc_bios_hle_vec_2 = 0x100 + 2;
+		const uint dc_bios_hle_vec_3 = 0x100 + 4;
+		const uint dc_bios_hle_vec_4 = 0x100 + 6;
+		const uint dc_bios_hle_vec_5 = 0x100 + 8;
+
+		const uint dc_bios_syscall_system = 0x8C0000B0;
+		const uint dc_bios_syscall_font = 0x8C0000B4;
+		const uint dc_bios_syscall_flashrom = 0x8C0000B8;
+		const uint dc_bios_syscall_GDrom_misc = 0x8C0000BC;
+		const uint dc_bios_syscall_resets_Misc = 0x8c0000e0;
+
 		static bool nobios;//no bios so everything has to be emulated(forced full hle)
 		static bool hle;//use hle emulation mixed mode (hle+bios)
+		static bool useBios = false;//use bios if it exists..
 		static byte[] bios_file = new byte[2*mb];// 2mb bios rom
 		static byte[] bios_flash = new byte[256*kb];//256 kb internal flash ram
-
+		static uint FakeRomFondAddr = 0x8cF82C18;//start of mem
 		public static int loadipbin(string name)//load and proc ip.bin/bootfile  -1/-2 on error
 		{
 			try//load ip.bin
@@ -62,6 +71,20 @@ namespace DC4Ever
 		}
 		public static void loadbiosfile(string name)//load bios file
 		{
+			//
+			try
+			{
+				System.IO.FileInfo fi = new System.IO.FileInfo("dcFont.bin");
+				System.IO.FileStream fs = fi.OpenRead();
+				byte[] fd = new byte[fi.Length];
+				fs.Read(fd, 0, (int)fi.Length);
+				for (uint i = 0; i < fi.Length; i++)
+				{
+					write(FakeRomFondAddr + i, fd[i], 1);
+				}
+				fs.Close();
+			}
+			catch { }
 			try//try to load the bios file..
 			{
 				System.IO.FileInfo fi= new System.IO.FileInfo(name);
@@ -79,6 +102,7 @@ namespace DC4Ever
 			}
 			hle=true;//Enable mixed mode (hle+bios)
 			dc.dcon.WriteLine("Using mixed Mode bios emulation (Bios+HLE)");
+
 		}
 		public static void loadbiosflashfile(string name)//load flash ram file
 		{
@@ -111,9 +135,11 @@ namespace DC4Ever
 				dc.dcon.WriteLine("Flash ram cannot be saved to \"" + name + "\"");
 			}
 		}
-		public static unsafe uint readBios(uint adr,int len)//read from bios mem - no write (rom)
+		static unsafe uint readBios(uint adr,int len)//read from bios mem - no write (rom)
 		{
-			//TODO : BIOS HLE Force and Mixed modes
+			
+            //return 0xB;
+            //TODO : BIOS HLE Force and Mixed modes
 			if (len ==0x3)//olny if this read is from an opcode fetch
 			{
 				#region Bios HLE Mixed
@@ -125,10 +151,121 @@ namespace DC4Ever
 				#region Bios HLE Forced
 				//if command is not hle and we do not have the bios 
 				//we _must_ emulate it..
-				if (nobios==true)
+				if (nobios==true | useBios==false)
 				{
+					//
 					//return a value according to the addr/register data
-					return 65535+100;//just jump back- no emulation :P
+					switch (pc)
+					{
+						case dc_bios_hle_vec_1:
+							switch (r[7])
+							{
+								case 0:
+									//SYSINFO_INIT 
+									WriteLine("Bios call : SYSINFO_INIT");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+								case 1:
+									//not a valid syscall 
+									WriteLine("Bios call : not a valid syscall");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+								case 2:
+									//SYSINFO_ICON 
+									WriteLine("Bios call : SYSINFO_ICON");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+								case 3:
+									//SYSINFO_ID 
+									WriteLine("Bios call : SYSINFO_ID");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+							}
+							break;
+						case dc_bios_hle_vec_2:
+							switch (r[1])
+							{
+								case 0:
+									//ROMFONT_ADDRESS 
+									//WriteLine("bios call : ROMFONT_ADDRESS");
+									r[0] = FakeRomFondAddr;
+									//WriteLine("Bios call was HLE'd");
+									return 65537;//just jump back- no emulation :P
+									//break;
+								case 1:
+									//ROMFONT_LOCK 
+									WriteLine("Bios call : ROMFONT_LOCK");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+								case 2:
+									//ROMFONT_UNLOCK 
+									WriteLine("Bios call : ROMFONT_UNLOCK");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+							}
+							break;
+						case dc_bios_hle_vec_3:
+							switch (r[7])
+							{
+								case 0:
+									//FLASHROM_INFO  
+									WriteLine("Bios call : FLASHROM_INFO");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+								case 1:
+									//FLASHROM_READ  
+									WriteLine("Bios call : FLASHROM_READ");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+								case 2:
+									//FLASHROM_WRITE  
+									WriteLine("Bios call : FLASHROM_WRITE");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+								case 3:
+									//FLASHROM_DELETE  
+									WriteLine("Bios call : FLASHROM_DELETE");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+							}
+							break;
+						case dc_bios_hle_vec_4:
+							switch (r[6])
+							{
+								case 0xFFFFFFFF:
+									//MISC superfunction 
+									WriteLine("Bios call : MISC superfunction");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+								case 0:
+									//GDROM superfunction 
+									WriteLine("Bios call : GDROM superfunction ");
+									WriteLine("Bios call was skiped");
+									return 65537;//just jump back- no emulation :P
+									//break;
+
+							}
+							break;
+						case dc_bios_hle_vec_5:
+							{
+								WriteLine("Bios call : unkoen system call");
+								WriteLine("Bios call was skiped");
+								return 65537;//just jump back- no emulation :P
+							}
+					}
+					//return 65537;
+					WriteLine("ReadBios (" + adr + ") at pc " + pc);
 				}
 				#endregion
 			}
@@ -150,7 +287,7 @@ namespace DC4Ever
 			dc.dcon.WriteLine("Wrong read size in readBios (" + len+") at pc "+pc);
 			return 0;
 		}
-		public static unsafe uint readBios_falsh(uint adr,int len)
+		static unsafe uint readBios_falsh(uint adr,int len)
 		{
 			switch (len)
 			{
@@ -166,7 +303,7 @@ namespace DC4Ever
 			dc.dcon.WriteLine("Wrong read size in readBios_flash (" + len+") at pc "+pc);
 			return 0;
 		}
-		public static unsafe void writeBios_flash(uint adr,uint data,int len)//write to the flash
+		static unsafe void writeBios_flash(uint adr,uint data,int len)//write to the flash
 		{
 			switch (len)
 			{
@@ -184,5 +321,15 @@ namespace DC4Ever
 			}
 			dc.dcon.WriteLine("Wrong write size in writeBios_flash (" + len+") at pc "+pc);
 		}
-	}
+
+        static void UpdateBios(uint cycles) { }
+		static void InitBios()
+		{
+			write(dc_bios_syscall_system , dc_bios_hle_vec_1, 4);
+			write(dc_bios_syscall_font, dc_bios_hle_vec_2, 4);
+			write(dc_bios_syscall_flashrom, dc_bios_hle_vec_3, 4);
+			write(dc_bios_syscall_GDrom_misc, dc_bios_hle_vec_4, 4);
+			write(dc_bios_syscall_resets_Misc, dc_bios_hle_vec_5, 4);
+		}
+    }
 }
