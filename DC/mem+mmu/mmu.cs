f7b2f825b72501ab53feecdaa404f61bc506671a
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2005-06-14 15:50:38 +0200

    Modified mmu.cs (DC4Ever (latest))

--- DC/mem+mmu/mmu.cs
+++ DC/mem+mmu/mmu.cs
@@ -16,15 +16,28 @@ namespace DC4Ever
 	/// MMU is emulated here (Memory managment usint)
 	/// Also some simple adress proc may be done here
 	/// </summary>
-    #if nrt 
     public static partial  class emu 
- #else  
-    public partial  class emu 
- #endif
     {
-		public static uint mmutrans(uint adr)
+		public static uint mmutrans(uint adr,int size)
 		{
-			return adr;//no mmu that simple..
+            switch (size)
+            {
+                case 1:
+                    return adr;
+                case 2:
+                    if ((adr & 0x1) != 0)
+                        WriteLine("Incorect addres , 2*k not folowed");
+                    return adr;
+				case 3:
+					if ((adr & 0x1) != 0)
+						WriteLine("Incorect addres , 2*k not folowed");
+					return adr;
+                case 4:
+                    if ((adr&0x3)!=0)
+                        WriteLine("Incorect addres , 4*k not folowed");
+                    return adr;
+            }
+            return adr;//no mmu that simple..
 		}
 
 	}
