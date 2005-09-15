08daa798e76201a65739aa3ff1afacded2292782
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2005-09-15 23:21:56 +0200

    Modified intexc.cs (dc4ever-2003)

--- DC/SH4.7091/intexc.cs
+++ DC/SH4.7091/intexc.cs
@@ -9,7 +9,7 @@
 //
 //#define Old_IntCCode
 using System;
-using System.Collections.Generic;
+using System.Collections;
 
 //SET_BIT(reg, bit) (reg |= (bit))
 //REMOVE_BIT(reg, bit) (reg &= ~(bit))
@@ -23,85 +23,109 @@ namespace DC4Ever
 	//The Interupt input to RL is form the holly sub system (the output of the holly interupt controler)
 	//The holly manages all external interupts , and it has 3 types of interupts normal , external and error
 
+	public enum InteruptType
+	{
+		sh4_int   = 0x00000000,
+		sh4_exp   = 0x01000000,
+		holly_nrm = 0x20000000,
+		holly_ext = 0x21000000,
+		holly_err = 0x22000000,
+		InteruptTypeMask = 0x7F000000,
+		InteruptIDMask=0x00FFFFFF
+	}
+
+
+
+	public enum sh4_int
+	{
+		//sh4 internal module interupts
+		sh4_in_TMU0=InteruptType.sh4_int|1,
+		sh4_in_TMU1=InteruptType.sh4_int|2,
+		sh4_in_TMU2=InteruptType.sh4_int|3,
+
+		//sh4 exeptions 
+		sh4_ex_USER_BREAK_BEFORE_INSTRUCTION_EXECUTION = InteruptType.sh4_exp | 0x1e0,
+		sh4_ex_INSTRUCTION_ADDRESS_ERROR =InteruptType.sh4_exp | 0x0e0,
+		sh4_ex_INSTRUCTION_TLB_MISS =InteruptType.sh4_exp | 0x040,
+		sh4_ex_INSTRUCTION_TLB_PROTECTION_VIOLATION = InteruptType.sh4_exp |0x0a0,
+		sh4_ex_GENERAL_ILLEGAL_INSTRUCTION = InteruptType.sh4_exp |0x180,
+		sh4_ex_SLOT_ILLEGAL_INSTRUCTION = InteruptType.sh4_exp |0x1a0,
+		sh4_ex_GENERAL_FPU_DISABLE = InteruptType.sh4_exp |0x800,
+		sh4_ex_SLOT_FPU_DISABLE = InteruptType.sh4_exp |0x820,
+		sh4_ex_DATA_ADDRESS_ERROR_READ =InteruptType.sh4_exp |0x0e0,
+		sh4_ex_DATA_ADDRESS_ERROR_WRITE = InteruptType.sh4_exp | 0x100,
+		sh4_ex_DATA_TLB_MISS_READ = InteruptType.sh4_exp | 0x040,
+		sh4_ex_DATA_TLB_MISS_WRITE = InteruptType.sh4_exp | 0x060,
+		sh4_ex_DATA_TLB_PROTECTION_VIOLATION_READ = InteruptType.sh4_exp | 0x0a0,
+		sh4_ex_DATA_TLB_PROTECTION_VIOLATION_WRITE = InteruptType.sh4_exp | 0x0c0,
+		sh4_ex_FPU = InteruptType.sh4_exp | 0x120,
+		sh4_ex_TRAP = InteruptType.sh4_exp | 0x160,
+		sh4_ex_INITAL_PAGE_WRITE = InteruptType.sh4_exp | 0x080,
+
+		// asic9a /sh4 external holly normal [internal]
+		holly_RENDER_DONE = InteruptType.holly_nrm | 0x02,
+		holly_SCANINT1 = InteruptType.holly_nrm | 0x03,
+		holly_SCANINT2 = InteruptType.holly_nrm | 0x04,
+		holly_VBLank = InteruptType.holly_nrm | 0x05,
+		holly_OPAQUE = InteruptType.holly_nrm | 0x07,
+		holly_OPAQUEMOD = InteruptType.holly_nrm | 0x08,
+		holly_TRANS = InteruptType.holly_nrm | 0x09,
+		holly_TRANSMOD = InteruptType.holly_nrm | 0x0a,
+		holly_MAPLE_DMA = InteruptType.holly_nrm | 0x0c,
+		holly_MAPLE_ERR = InteruptType.holly_nrm | 0x0d,
+		holly_GDROM_DMA = InteruptType.holly_nrm | 0x0e,
+		holly_SPU_DMA = InteruptType.holly_nrm | 0x0f,
+		holly_PVR_DMA = InteruptType.holly_nrm | 0x13,
+		holly_PUNCHTHRU = InteruptType.holly_nrm | 0x15,
+
+		// asic9c/sh4 external holly external [EXTERNAL]
+		holly_GDROM_CMD = InteruptType.holly_ext | 0x00,
+		holly_SPU_IRQ = InteruptType.holly_ext | 0x01,
+		holly_EXP_8BIT = InteruptType.holly_ext | 0x02,
+		holly_EXP_PCI = InteruptType.holly_ext | 0x03,
+
+		// asic9b/sh4 external holly err only error [error]
+		holly_PRIM_NOMEM = InteruptType.holly_err | 0x02,
+		holly_MATR_NOMEM = InteruptType.holly_err | 0x03
+	}
+
+
+	public class Queue_sh4_int_:Queue 
+	{
+		public new sh4_int Dequeue()
+		{
+			return (sh4_int)base.Dequeue ();
+		}
+		public new sh4_int Peek()
+		{
+			return (sh4_int)base.Peek ();
+		}
+		public void Enqueue(sh4_int obj)
+		{
+			base.Enqueue (obj);
+		}
+
+
+
+	}
 
 	/// <summary>
 	/// Interups ,Exeptions ,Resets Handling
 	/// </summary>
-	public static unsafe partial class emu
+	public unsafe class intc
 	{
 
-		public enum InteruptType
-		{
-			sh4_int   = 0x00000000,
-			sh4_exp   = 0x01000000,
-			holly_nrm = 0x20000000,
-			holly_ext = 0x21000000,
-			holly_err = 0x22000000,
-			InteruptTypeMask = 0x7F000000,
-			InteruptIDMask=0x00FFFFFF
-		}
-
-
-		public enum sh4_int
-		{
-			//sh4 internal module interupts
-
-			//sh4 exeptions 
-			sh4_ex_USER_BREAK_BEFORE_INSTRUCTION_EXECUTION = InteruptType.sh4_exp | 0x1e0,
-			sh4_ex_INSTRUCTION_ADDRESS_ERROR =InteruptType.sh4_exp | 0x0e0,
-			sh4_ex_INSTRUCTION_TLB_MISS =InteruptType.sh4_exp | 0x040,
-			sh4_ex_INSTRUCTION_TLB_PROTECTION_VIOLATION = InteruptType.sh4_exp |0x0a0,
-			sh4_ex_GENERAL_ILLEGAL_INSTRUCTION = InteruptType.sh4_exp |0x180,
-			sh4_ex_SLOT_ILLEGAL_INSTRUCTION = InteruptType.sh4_exp |0x1a0,
-			sh4_ex_GENERAL_FPU_DISABLE = InteruptType.sh4_exp |0x800,
-			sh4_ex_SLOT_FPU_DISABLE = InteruptType.sh4_exp |0x820,
-			sh4_ex_DATA_ADDRESS_ERROR_READ =InteruptType.sh4_exp |0x0e0,
-			sh4_ex_DATA_ADDRESS_ERROR_WRITE = InteruptType.sh4_exp | 0x100,
-			sh4_ex_DATA_TLB_MISS_READ = InteruptType.sh4_exp | 0x040,
-			sh4_ex_DATA_TLB_MISS_WRITE = InteruptType.sh4_exp | 0x060,
-			sh4_ex_DATA_TLB_PROTECTION_VIOLATION_READ = InteruptType.sh4_exp | 0x0a0,
-			sh4_ex_DATA_TLB_PROTECTION_VIOLATION_WRITE = InteruptType.sh4_exp | 0x0c0,
-			sh4_ex_FPU = InteruptType.sh4_exp | 0x120,
-			sh4_ex_TRAP = InteruptType.sh4_exp | 0x160,
-			sh4_ex_INITAL_PAGE_WRITE = InteruptType.sh4_exp | 0x080,
-
-			// asic9a /sh4 external holly normal [internal]
-			holly_RENDER_DONE = InteruptType.holly_nrm | 0x02,
-			holly_SCANINT1 = InteruptType.holly_nrm | 0x03,
-			holly_SCANINT2 = InteruptType.holly_nrm | 0x04,
-			holly_VBLank = InteruptType.holly_nrm | 0x05,
-			holly_OPAQUE = InteruptType.holly_nrm | 0x07,
-			holly_OPAQUEMOD = InteruptType.holly_nrm | 0x08,
-			holly_TRANS = InteruptType.holly_nrm | 0x09,
-			holly_TRANSMOD = InteruptType.holly_nrm | 0x0a,
-			holly_MAPLE_DMA = InteruptType.holly_nrm | 0x0c,
-			holly_MAPLE_ERR = InteruptType.holly_nrm | 0x0d,
-			holly_GDROM_DMA = InteruptType.holly_nrm | 0x0e,
-			holly_SPU_DMA = InteruptType.holly_nrm | 0x0f,
-			holly_PVR_DMA = InteruptType.holly_nrm | 0x13,
-			holly_PUNCHTHRU = InteruptType.holly_nrm | 0x15,
-
-			// asic9c/sh4 external holly external [EXTERNAL]
-			holly_GDROM_CMD = InteruptType.holly_ext | 0x00,
-			holly_SPU_IRQ = InteruptType.holly_ext | 0x01,
-			holly_EXP_8BIT = InteruptType.holly_ext | 0x02,
-			holly_EXP_PCI = InteruptType.holly_ext | 0x03,
-
-			// asic9b/sh4 external holly err only error [error]
-			holly_PRIM_NOMEM = InteruptType.holly_err | 0x02,
-			holly_MATR_NOMEM = InteruptType.holly_err | 0x03
-		}
-
-		const int IPr_LVL6 = 0x5;
-		const int IPr_LVL4 = 0x3;
-		const int IPr_LVL2 = 0x1;
-
-		static Queue<sh4_int> pending_interups = new Queue<sh4_int>();
-
-		static bool zleeping = false, awake = false;
-		static int pvr_registered = 0;
 		
-		static void UpdateIntExc(uint cycles)
+		const int IPr_LVL6 = 0x6;
+		const int IPr_LVL4 = 0x4;
+		const int IPr_LVL2 = 0x2;
+
+		static Queue_sh4_int_ pending_interups = new Queue_sh4_int_();
+
+		public static bool zleeping = false, awake = false;
+		//static int pvr_registered = 0;
+		
+		public static void UpdateIntExc(uint cycles)
 		{
 			check_ints();
 		}
@@ -115,7 +139,7 @@ namespace DC4Ever
 
 		static bool check_ints()
 		{
-			if (pc_funct != 0)
+			if (sh4.pc_funct != 0)
 				return false;
 
 			
@@ -127,27 +151,35 @@ namespace DC4Ever
 
 			InteruptType type = (InteruptType)((uint)interupt & (uint)InteruptType.InteruptTypeMask);
 
-
+			bool rv=false;
 
 			switch (type)
 			{
 				case InteruptType.sh4_int:
-					return HandleSH4_int((sh4_int)((uint)interupt));
+					rv= HandleSH4_int(interupt);
+					break;
 
 				case InteruptType.sh4_exp:
-					return HandleSH4_exept((sh4_int)((uint)interupt ));
+					rv= HandleSH4_exept(interupt);
+					break;
 
 				case InteruptType.holly_nrm:
-					return HandleHolly_nrm((sh4_int)((uint)interupt));
+					rv= HandleHolly_nrm(interupt);
+					break;
 
 				case InteruptType.holly_ext:
-					return HandleHolly_ext((sh4_int)((uint)interupt));
+					rv= HandleHolly_ext(interupt);
+					break;
 
 				case InteruptType.holly_err:
-					return HandleHolly_err((sh4_int)((uint)interupt ));
+					rv= HandleHolly_err(interupt);
+					break;
 			}
 
-			return false;
+			if (!rv)
+				pending_interups.Enqueue(interupt);
+
+			return rv;
 /*
 			if (((interupt >> 24) & 0xFF) != 0)
 			{
@@ -212,25 +244,35 @@ namespace DC4Ever
 
 		static bool HandleSH4_int(sh4_int intpt)
 		{
+			switch(intpt)
+			{
+				case  sh4_int.sh4_in_TMU0:
+					return Do_interupt((uint)(((*mem.IPRA) >> 12)&0xf),0x400,0x600);
+				case  sh4_int.sh4_in_TMU1:
+					return Do_interupt((uint)(((*mem.IPRA) >> 8)&0xf),0x420,0x600);
+				case  sh4_int.sh4_in_TMU2:
+					return Do_interupt((uint)(((*mem.IPRA) >> 4)&0xf),0x440,0x600);
+			}
+
 			return false;
 		}
 
 		static bool HandleHolly_nrm(sh4_int intpt)
 		{
 			uint interupt = (uint)(1 << (((int)((uint)intpt & (uint)InteruptType.InteruptIDMask))));
-			*ISTNRM |= interupt;
+			*mem.ISTNRM |= interupt;
 
-			if ((*IML6NRM & interupt) != 0)
+			if ((*mem.IML6NRM & interupt) != 0)
 			{
 				if (Do_interupt(IPr_LVL6, 0x320, 0x600))
 					return true;
 			}
-			else if ((*IML4NRM & interupt) != 0)
+			else if ((*mem.IML4NRM & interupt) != 0)
 			{
 				if (Do_interupt(IPr_LVL4, 0x360, 0x600))
 					return true;
 			}
-			else if ((*IML2NRM & interupt) != 0)
+			else if ((*mem.IML2NRM & interupt) != 0)
 			{
 				if (Do_interupt(IPr_LVL2, 0x3A0, 0x600))
 					return true;
@@ -242,19 +284,19 @@ namespace DC4Ever
 		static bool HandleHolly_ext(sh4_int intpt)
 		{
 			uint interupt = (uint)(1 << ((int)((uint)intpt & (uint)InteruptType.InteruptIDMask)));
-			*ISTEXT |= interupt;
+			*mem.ISTEXT |= interupt;
 
-			if ((*IML6EXT & interupt) != 0)
+			if ((*mem.IML6EXT & interupt) != 0)
 			{
 				if (Do_interupt(IPr_LVL6, 0x320, 0x600))
 					return true;
 			}
-			else if ((*IML4EXT & interupt) != 0)
+			else if ((*mem.IML4EXT & interupt) != 0)
 			{
 				if (Do_interupt(IPr_LVL4, 0x360, 0x600))
 					return true;
 			}
-			else if ((*IML2EXT & interupt) != 0)
+			else if ((*mem.IML2EXT & interupt) != 0)
 			{
 				if (Do_interupt(IPr_LVL2, 0x3a0, 0x600))
 					return true;
@@ -265,19 +307,19 @@ namespace DC4Ever
 		static bool HandleHolly_err(sh4_int intpt)
 		{
 			uint interupt = (uint)(1 << ((int)((uint)intpt & (uint)InteruptType.InteruptIDMask)));
-			*ISTERR |= interupt;
+			*mem.ISTERR |= interupt;
 
-			if ((*IML6ERR & interupt) != 0)
+			if ((*mem.IML6ERR & interupt) != 0)
 			{
 				if (Do_interupt(IPr_LVL6, 0x320, 0x600))
 					return true;
 			}
-			else if ((*IML4ERR & interupt) != 0)
+			else if ((*mem.IML4ERR & interupt) != 0)
 			{
 				if (Do_interupt(IPr_LVL4, 0x360, 0x600))
 					return true;
 			}
-			else if ((*IML2ERR & interupt) != 0)
+			else if ((*mem.IML2ERR & interupt) != 0)
 			{
 				if (Do_interupt(IPr_LVL2, 0x3a0, 0x600))
 					return true;
@@ -290,33 +332,33 @@ namespace DC4Ever
 
 			if (!zleeping)
 			{
-				if (sr.BL == 1 || vbr == 0)
+				if (sh4.sr.BL == 1 || sh4.vbr == 0)
 					return false;
 			}
 
 			//interupt disabled :)
-			if (sr.IMASK == 0xf || lvl == 0)
+			if (sh4.sr.IMASK == 0xf || lvl == 0)
 				return false;
 
 			//test interupt level
-			if (sr.IMASK > lvl)
+			if (sh4.sr.IMASK > lvl)
 				return false;
 
-			*INTEVT = intEvn;
+			*mem.INTEVT = intEvn;
 
 			if (zleeping)
 				awake = true;
 
-			ssr = sr.reg;
-			spc = pc;
-			sgr = r[15];
-			sr.BL = 1;
-			sr.MD = 1;
-			sr.RB = 1;
+			sh4.ssr = sh4.sr.reg;
+			sh4.spc = sh4.pc;
+			sh4.sgr = sh4.r[15];
+			sh4.sr.BL = 1;
+			sh4.sr.MD = 1;
+			sh4.sr.RB = 1;
 
-			cstAddCall(pc, pc, vbr + 0x600, CallType.Interupt);
+			CallStackTrace.cstAddCall(sh4.pc, sh4.pc, sh4.vbr + 0x600, CallType.Interupt);
 
-			pc = vbr + CallVect;
+			sh4.pc = sh4.vbr + CallVect;
 			
 			return true;
 		}
@@ -326,28 +368,28 @@ namespace DC4Ever
 
 			if (!zleeping)
 			{
-				if (sr.BL == 1 || vbr == 0)
+				if (sh4.sr.BL == 1 || sh4.vbr == 0)
 					return false;
 			}
 
-			if (sr.IMASK > lvl)
+			if (sh4.sr.IMASK > lvl)
 				return false;
 
-			*EXPEVT = expEvn;
+			*mem.EXPEVT = expEvn;
 
 			if (zleeping)
 				awake = true;
 
-			ssr = sr.reg;
-			spc = pc;
-			sgr = r[15];
-			sr.BL = 1;
-			sr.MD = 1;
-			sr.RB = 1;
+			sh4.ssr = sh4.sr.reg;
+			sh4.spc = sh4.pc;
+			sh4.sgr = sh4.r[15];
+			sh4.sr.BL = 1;
+			sh4.sr.MD = 1;
+			sh4.sr.RB = 1;
 
-			cstAddCall(pc, pc, vbr + 0x600, CallType.Interupt);
+			CallStackTrace.cstAddCall(sh4.pc, sh4.pc, sh4.vbr + 0x600, CallType.Interupt);
 
-			pc = vbr + CallVect;
+			sh4.pc = sh4.vbr + CallVect;
 
 			return true;
 		}
